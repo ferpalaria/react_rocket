46c206f197290c3634d3fda57625eb78a5ee4350
Fernanda <frpalaria@gmail.com>
2023-09-08 00:24:18 -0300

    fixed home style

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -6,7 +6,7 @@ export const Container = styled.div`
 
     display: grid;
     grid-template-columns: 250px auto;
-    grid-template-rows: 105px 128px, auto, 64px;
+    grid-template-rows: 105px 128px auto 64px;
     grid-template-areas: 
     "brand header"
     "menu search"
